ff5307aa7bbeb5b22f7cf22b08135f0237aa9c75
udayt139 <uday@totient.co.in>
2019-10-25 16:18:57 +0530

    w

---
+++ Test Suites/TestRail/New Dynamic Querying Test Suite.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>New Dynamic Querying Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8cf933e1-fbfa-48b1-a019-d61570d2ad5b</testSuiteGuid>
+   <filteringExtension>com.katalon.plugin.dynamic_execution.TestRailFilteringTestSuiteImpl</filteringExtension>
+   <filteringPlugin>com.katalon.katalon-studio-testrail-plugin</filteringPlugin>
+   <filteringText>3</filteringText>
+</FilteringTestSuiteEntity>
